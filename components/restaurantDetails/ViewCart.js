247b6d59d792eec3300cfd1775ad92229b8197b0
Linu <linushibu.ls@gmail.com>
2022-03-04 00:22:09 +0530

    View Cart and Total price

--- components/restaurantDetails/ViewCart.js
+++ components/restaurantDetails/ViewCart.js
@@ -1,16 +1,34 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { TouchableOpacity } from "react-native";
+import { useSelector } from "react-redux";
 
 const ViewCart = () => {
+  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const total = items
+    .map((item) => Number(item.price.replace("$", "")))
+    .reduce((prev, curr) => prev + curr, 0);
+
+  const totalUSD = total.toLocaleString("en", {
+    style: "currency",
+    currency: "USD",
+  });
+  console.log(totalUSD);
   return (
-    <View style={styles.view}>
-      <View style={styles.viewBtn}>
-        <TouchableOpacity style={styles.btn}>
-          <Text style={styles.text}>View Cart</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    <>
+      {total ? (
+        <View style={styles.view}>
+          <View style={styles.viewBtn}>
+            <TouchableOpacity style={styles.btn}>
+              <Text style={styles.text}>View Cart</Text>
+              <Text style={styles.text}>{`$${totalUSD}`}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
@@ -32,12 +50,15 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 15,
     color: "white",
+    marginRight: 20,
   },
   btn: {
     marginTop: 5,
     backgroundColor: "black",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    padding: 15,
     alignItems: "center",
-    padding: 10,
     borderRadius: 30,
     width: 200,
     position: "relative",
