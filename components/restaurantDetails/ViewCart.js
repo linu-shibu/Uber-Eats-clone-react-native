3f547ec2502e01abc4811aa615f755430d405fa2
Linu <linushibu.ls@gmail.com>
2022-03-05 01:46:30 +0530

    Adding Checkout modal

--- components/restaurantDetails/ViewCart.js
+++ components/restaurantDetails/ViewCart.js
@@ -1,9 +1,13 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Modal } from "react-native";
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import { set } from "react-native-reanimated";
 
 const ViewCart = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   const items = useSelector((state) => state.cartReducer.selectedItems.items);
   const total = items
     .map((item) => Number(item.price.replace("$", "")))
@@ -13,13 +17,36 @@ const ViewCart = () => {
     style: "currency",
     currency: "USD",
   });
-  console.log(totalUSD);
+
+  const checkoutModalContent = () => {
+    return (
+      <View style={styles.modalMainView}>
+        <View style={styles.modalView}>
+          <TouchableOpacity onPress={() => setModalVisible(false)}>
+            <Text style={{ color: "white" }}>Checkout</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+  // console.log(totalUSD);
   return (
     <>
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        transparent={true}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        {checkoutModalContent()}
+      </Modal>
       {total ? (
         <View style={styles.view}>
           <View style={styles.viewBtn}>
-            <TouchableOpacity style={styles.btn}>
+            <TouchableOpacity
+              style={styles.btn}
+              onPress={() => setModalVisible(true)}
+            >
               <Text style={styles.text}>View Cart</Text>
               <Text style={styles.text}>{`$${totalUSD}`}</Text>
             </TouchableOpacity>
@@ -62,6 +89,19 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     width: 200,
     position: "relative",
+  },
+  modalView: {
+    backgroundColor: "black",
+    padding: 10,
+    borderRadius: 30,
+    width: 150,
+    alignItems: "center",
+  },
+  modalMainView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
   },
 });
 
